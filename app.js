fbf51ce2c67535eb44f820142ef5392b52e26692
John Barhorst <crashdsinc@comcast.net>
2019-11-09 14:22:17 -0600

    solved that whole search thing. paramarama

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
 const express = require('express');
 const request = require('request');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const port = 3001;
-
-//Middleware
+const port = process.env.PORT || 3001;
 const API_KEY = process.env.API_KEY;
 
+//Middleware
+app.use(function (req, res, next) {
+  res.header("Origin", "https://www.bungie.net");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Origin", `http://localhost:${port}`);
+  next();
+});
 
+// app.use(cors());
 
-app.use('/test', function (req, res, next) {
-  console.log('Got there');
+//Handle Character Search Request 
+app.use('/api/search/:search', (req, res, next) => {
+  console.log('Search Character');
+  console.log(req.params);
   request({
-    url: `https://www.bungie.net/Platform/Destiny2/1/Profile/4611686018434143187/?components=200`,
+    url: `https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/All/${req.params.search}/`,
+    headers: {
+      "X-API-KEY": API_KEY
+    }
+  }).pipe(res);
+
+});
+
+
+// Handle Character List Request
+app.use('/api/:platformId/:accountId', (req, res, next) => {
+  console.log('Got there');
+  console.log(req.params);
+  request({
+    url: `https://www.bungie.net/Platform/Destiny2/${req.params.platformId}/Profile/${req.params.accountId}/?components=200`,
     headers: {
       "X-API-KEY": API_KEY
     }
@@ -27,8 +50,8 @@ app.use(function (err, req, res, next) {
 })
 // Routes
 
-app.get('/test', (req, res, next) => {
-
+app.get('/api/search/:search', (req, res, next) => {
+  console.log(res);
   res.send(res);
 });
 
